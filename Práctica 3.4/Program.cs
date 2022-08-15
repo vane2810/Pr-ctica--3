55fee6fe106901c81f234c5a227466868e01d925
vane2810 <olgavanessasf@gmail.com>
2022-08-15 17:05:04 -0600

    4° proyecto

---
+++ Práctica 3.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Práctica_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool condition = true;
+            int cont = 0;
+
+            Console.WriteLine("Ingrese el número de repeticiones que desea: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            while (condition)
+            {
+                if (cont == limite)
+                    condition = false;
+
+                Console.WriteLine(cont);
+                cont++;
+
+            }
+        }
+    }
+}
