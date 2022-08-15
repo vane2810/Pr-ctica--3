ca01744083bc54baa9e6e95f130e55c3a76d8696
vane2810 <olgavanessasf@gmail.com>
2022-08-15 16:53:29 -0600

    Agregar archivos de proyecto.

---
+++ Práctica  3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Práctica__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
