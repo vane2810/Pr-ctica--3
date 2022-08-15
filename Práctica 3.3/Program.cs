ac0bf092e08e0313e351a48e0af827ef24a01188
vane2810 <olgavanessasf@gmail.com>
2022-08-15 17:00:35 -0600

    3° proyecto

---
+++ Práctica 3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Práctica_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número a procesar :");
+            int multiplo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el límite :");
+            int limite = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= limite; i++)
+            {
+                if (i % multiplo == 0)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+        }
+    }
+}
