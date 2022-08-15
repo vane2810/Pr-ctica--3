4a4cb82ef97eef3d975704e903d3523ac0b37ae7
vane2810 <olgavanessasf@gmail.com>
2022-08-15 16:56:15 -0600

    1°

--- Práctica  3/Program.cs
+++ Práctica  3/Program.cs
@@ -6,7 +6,29 @@ namespace Práctica__3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese su nota promedio:");
+            double nota = double.Parse(Console.ReadLine());
+            if (nota >= 0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                }
+
+                else if (nota >= 5)
+                {
+                    Console.WriteLine("Reposición");
+                }
+
+                else
+                {
+                    Console.WriteLine("Reprobado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La nota no es correcta");
+            }
         }
     }
 }
